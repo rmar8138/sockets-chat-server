41bd74326959ac4329e8da7935aa8d1c640faf83
rmar8138 <ragan.martinez@live.com>
2019-12-21 16:34:01 +1100

    Configure cors for routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
+
+// enable cors
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 router.get("/", (req, res) => res.send("Hello World!"));
 router.get("/goodbye", (req, res) => res.send("Goodbye World!"));
