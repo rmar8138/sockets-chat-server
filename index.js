25d65188afdf792b3edb7388f38325a576e34693
rmar8138 <ragan.martinez@live.com>
2019-12-21 15:31:17 +1100

    Modularize routes and app to separate files

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT || 5000;
-
-mongoose.connect(`mongodb://localhost/sockets-chat`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
