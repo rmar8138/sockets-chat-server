0b5749e175462c713f72447e77d1db543d9223e1
rmar8138 <ragan.martinez@live.com>
2019-12-22 14:40:25 +1100

    Move sockets function back to index to fix later, and emit render message to all sockets

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const app = require("./app");
 const http = require("http").createServer(app);
-const socketHandler = require("./sockets");
+const MessageModel = require("./database/models/MessageModel");
 const port = process.env.PORT || 5000;
 
 const io = require("socket.io")(http);
 
-io.on("connection", socketHandler);
+io.on("connection", socket => {
+  console.log("Someone has connected!");
+
+  // receive message and save to db
+  socket.on("sendMessage", async data => {
+    const { body } = data;
+    const newMessage = await MessageModel.create({
+      body,
+      socket: socket.id,
+      name: "test"
+    });
+
+    io.emit("renderMessage", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Someone has left");
+  });
+});
 
 http.listen(port, () => console.log(`Server listening on port ${port}`));
+
+module.exports = { io };
